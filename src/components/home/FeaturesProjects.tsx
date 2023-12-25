c83f55d6c7f1a31fe5ae20a3f94c55f1006595e9
Carlos Zambrano <emmanuelzzz123@gmail.com>
2023-12-25 16:36:58 -0700

    ecomerse add

--- src/components/home/FeaturesProjects.tsx
+++ src/components/home/FeaturesProjects.tsx
@@ -16,7 +16,7 @@ interface ExpItemProps {
   item: Exp
 }
 
-const elements: Array<Exp> = [
+const elements1: Array<Exp> = [
   {
     label: 'Java, JS, JSX, TypeScript, Kotlin ',
     value: 'PROGRAMMER',
@@ -31,6 +31,54 @@ const elements: Array<Exp> = [
     label: 'Node, SQL, NoSQL, MongoDB',
     value: 'BACKEND',
     photo: '/images/juegorol3.PNG',
+  },
+]
+
+const elements2: Array<Exp> = [
+  {
+    label: 'Java, JS, JSX, TypeScript, Kotlin ',
+    value: 'PROGRAMMER',
+    photo: '/images/ecomerse1.PNG',
+  },
+  {
+    label: 'HTML5, ReactJS, Vue, NEXT ,Tailwind, SASS, Bootstrap, MUI, PWA-SPA, Redux',
+    value: 'FRONTEND',
+    photo: '/images/ecomerse2.PNG',
+  },
+  {
+    label: 'Node, SQL, NoSQL, MongoDB',
+    value: 'BACKEND',
+    photo: '/images/ecomerse3.PNG',
+  },
+  {
+    label: 'HTML5, ReactJS, Vue, NEXT ,Tailwind, SASS, Bootstrap, MUI, PWA-SPA, Redux',
+    value: 'FRONTEND',
+    photo: '/images/ecomerse4.PNG',
+  },
+  {
+    label: 'Node, SQL, NoSQL, MongoDB',
+    value: 'BACKEND',
+    photo: '/images/ecomerse5.PNG',
+  },
+  {
+    label: 'HTML5, ReactJS, Vue, NEXT ,Tailwind, SASS, Bootstrap, MUI, PWA-SPA, Redux',
+    value: 'FRONTEND',
+    photo: '/images/ecomerse6.PNG',
+  },
+  {
+    label: 'Node, SQL, NoSQL, MongoDB',
+    value: 'BACKEND',
+    photo: '/images/ecomerse7.PNG',
+  },
+  {
+    label: 'HTML5, ReactJS, Vue, NEXT ,Tailwind, SASS, Bootstrap, MUI, PWA-SPA, Redux',
+    value: 'FRONTEND',
+    photo: '/images/ecomerse8.PNG',
+  },
+  {
+    label: 'Node, SQL, NoSQL, MongoDB',
+    value: 'BACKEND',
+    photo: '/images/ecomerse.PNG',
   },
 ]
 
@@ -67,23 +115,61 @@ const FeaturesProjects: FC = () => {
         <Typography variant="h1" sx={{ position: 'relative', fontSize: 40, textAlign: 'left' }} id="section3">
           <FavoriteIcon sx={{ width: 30, height: 30 }} /> Feature Projects
         </Typography>
+        {/* ECOMERSE */}
         <Box>
           <Carousel
             sx={{ width: '100%' }}
-            // navButtonsWrapperProps="false"
-            // navButtonsProps={false}
             stopAutoPlayOnHover={false}
             autoPlay={true}
             animation="fade"
-            duration={500}
-            interval={5000}
+            duration={1000}
+            interval={3000}
             indicators={false}
             navButtonsAlwaysInvisible={true}
-            // navButtonsAlwaysVisible={true}
-            // next={() => {}}
-            // prev={() => {}}
           >
-            {elements.map((item) => (
+            {elements2.map((item) => (
+              <Grid key={item.value}>
+                <ExpItem item={item} />
+              </Grid>
+            ))}
+          </Carousel>
+          <Box sx={{ mt: 1 }}>
+            <img src={'./images/icons/icon-mui.png'} alt={'React'} height={'35px'} />
+            <img src={'./images/icons/reactlogo.png'} alt={'React'} height={'35px'} />
+            <img src={'./images/icons/icon-js.png'} alt={'React'} height={'35px'} />
+            <img src={'./images/icons/icon-chatgpt.png'} alt={'React'} height={'30px'} />
+            <Typography color="text.secondary" variant="h5" sx={{ mt: 0 }}>
+              <a href="https://zam-ecommerce-demo.netlify.app/#/">https://zam-ecommerce-demo.netlify.app/#/</a>
+            </Typography>
+          </Box>
+          <Typography
+            color="text.secondary"
+            variant="h4"
+            sx={{ mt: 3, mx: { xl: 20, lg: 15, md: 10, sm: 5, xs: 0 } }}
+            align="justify"
+          >
+            This web application is a fully functional PWA, it's my personal project and applies everything I've
+            learned. It uses React and Vite, implemented with the assistance of UI-material components. It includes a
+            functional REST API structure and has sections for product and user administration. It utilizes MongoDB and
+            third-party services like Firebase and Oauth
+          </Typography>
+        </Box>
+        <br />
+        <hr />
+        <br />
+        {/* GAME IA */}
+        <Box>
+          <Carousel
+            sx={{ width: '100%' }}
+            stopAutoPlayOnHover={false}
+            autoPlay={true}
+            animation="fade"
+            duration={1000}
+            interval={3000}
+            indicators={false}
+            navButtonsAlwaysInvisible={true}
+          >
+            {elements1.map((item) => (
               <Grid key={item.value}>
                 <ExpItem item={item} />
               </Grid>
